cfb1e6887e9b2ab8ae6651ed1bc466887fc9d343
Aleksey <44026295+azhlbn@users.noreply.github.com>
2022-04-27 13:35:23 +0300

    Create pools.js

---
+++ pools.js
@@ -0,0 +1,35 @@
+// >>> CAKE-WBNB bscMainnet
+const cakePair = "0x0eD7e52944161450477ee417DE9Cd3a859b14fD0" // WBNB-CAKE
+const rewardToken = "0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82" // CAKE
+const masterChefPancake = "0x73feaa1eE314F8c655E354234017bE2193C9E24E" ;
+const pancakeRouter = "0x10ED43C718714eb63d5aA57B78B54704E256024E";
+const pancakePID = 251;
+const wBNBToken = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
+
+// >>> BSW-WBNB bscMainnet
+const cakePair = "0x8CA3fF14A52b080C54A6d1a405eecA02959d39fE" // WBNB-BSW
+const rewardToken = "0x965F527D9159dCe6288a2219DB51fc6Eef120dD1" // BSW
+const masterChefPancake = "0x73feaa1eE314F8c655E354234017bE2193C9E24E" ;
+const pancakeRouter = "0x10ED43C718714eb63d5aA57B78B54704E256024E";
+const pancakePID = 516;
+
+// >>> BUSD-WBNB
+const cakePair = "0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16" // WBNB-BSW
+const rewardToken = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56" //
+const masterChefPancake = "0x73feaa1eE314F8c655E354234017bE2193C9E24E" ;
+const pancakeRouter = "0x10ED43C718714eb63d5aA57B78B54704E256024E";
+const pancakePID = 252;
+
+// >>> CEEK-WBNB
+const cakePair = "0x046A9B3A9b743340eE2Bc4C6dDD35543E237C6c2" // WBNB-CEEK
+const rewardToken = "0xe0F94Ac5462997D2BC57287Ac3a3aE4C31345D66" // CEEK
+const masterChefPancake = "0x73feaa1eE314F8c655E354234017bE2193C9E24E" ;
+const pancakeRouter = "0x10ED43C718714eb63d5aA57B78B54704E256024E";
+const pancakePID = 523;
+
+// >>> PEX-WBNB
+const cakePair = "0x5ca96E8bDe0Bc587DaC9e02422Fd205b1102DAa4" // WBNB-PEX
+const rewardToken = "0x6a0b66710567b6beb81A71F7e9466450a91a384b" // PEX
+const masterChefPancake = "0x73feaa1eE314F8c655E354234017bE2193C9E24E" ;
+const pancakeRouter = "0x10ED43C718714eb63d5aA57B78B54704E256024E";
+const pancakePID = 520;
